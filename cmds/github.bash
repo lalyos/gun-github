f3b524acb5c865722b1ec364a1561928cc44fd41
lalyos <lalyos@yahoo.com>
2017-02-02 11:52:31 +0100

    Add starter Gunfile

--- cmds/github.bash
+++ cmds/github.bash
@@ -2,13 +2,13 @@ init() {
     deps-require jq
     
     cmd-export gh-orgs
-    # go to https://github.com/settings/tokens to create one
     env-import GITHUB_TOKEN
 }
 
 gh-orgs() {
     declare desc="Lists your GithHub organizations"
 
+    : ${GITHUB_TOKEN:? go to https://github.com/settings/tokens to create one}
     curl -s \
         -H "Authorization: Bearer $GITHUB_TOKEN" \
         https://api.github.com/user/orgs \
